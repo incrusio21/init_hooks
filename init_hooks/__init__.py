cd850c26c257b02b57df695d76f792f82567cc0b
ariefrdika <arief.rian87@gmail.com>
2024-07-15 11:54:37 +0000

    fix: memastikan ketika get app tidak error

--- init_hooks/__init__.py
+++ init_hooks/__init__.py
@@ -2,16 +2,19 @@ import frappe
 
 __version__ = '0.0.1'
 
+def module_name(method_str, is_getattr=False):
+    module_path, method_name = method_str.rsplit(".", 1)
+    module = frappe.get_module(module_path)
+    if not hasattr(module, method_name):
+        raise ImportError(
+            "method {0} does not exist in module {1}".format(method_name, module_path)
+        )
+    
+    return ["", getattr(module, method_name)] if is_getattr else [module, method_name]
+
 def get_custom_method():
-    def module_name(method_str, is_getattr=False):
-        module_path, method_name = method_str.rsplit(".", 1)
-        module = frappe.get_module(module_path)
-        if not hasattr(module, method_name):
-            raise ImportError(
-                "method {0} does not exist in module {1}".format(method_name, module_path)
-            )
-        
-        return ["", getattr(module, method_name)] if is_getattr else [module, method_name]
+    if not hasattr(frappe.local, "conf") or not frappe.db:
+        return
     
     for method, over_method in frappe.get_hooks("override_methods_custom", {}).items():
         from_module, from_method = module_name(method)
